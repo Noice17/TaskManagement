93c675fb977a25f75f2c77505b17392d2051b636
mpenalosa <ionpnls@gmail.com>
2025-08-02 13:26:00 +0800

    Update cors to localhost:3000

--- src/main/java/com/tms/TaskManagement/config/SecurityConfig.java
+++ src/main/java/com/tms/TaskManagement/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://chic-integrity-production.up.railway.app", "http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://chic-integrity-production.up.railway.app", "http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
